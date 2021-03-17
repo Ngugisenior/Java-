d6b31c280cf911ba8f2cf64b9d26c2d65c782416
Ngugisenior <ngugijoseph992@gmail.com>
2021-03-17 07:28:46 +0100

    updates

---
+++ Vaadin/ProductListing/src/main/java/com/pyrax/ProductListing/ProductListingApplication.java
@@ -0,0 +1,13 @@
+package com.pyrax.ProductListing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductListingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductListingApplication.class, args);
+	}
+
+}
